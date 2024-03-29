47833a803fdfce0684733e73ba31924f36ddb5f3
aVALKY <Fillierelouis1@gmail.com>
2024-03-29 16:25:03 +0100

    Ajout du rank du joueur en dessous de son niveau de compte

--- src/App.js
+++ src/App.js
@@ -7,19 +7,25 @@ import './App.css';
 function App() {
 const [SearchText, setSearchText] = useState("");
 const [playerData, setPlayerData] = useState({});
+const [playerRank, setPlayerRank] = useState ({});
+
 const API_KEY ="RGAPI-348637b2-be89-4132-89a1-b6bbf0c27132"
 
 function searchForPlayer (event){
   var APIcall = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ SearchText + "?api_key=" + API_KEY;
   
   axios.get(APIcall).then(function (response) {
-    // ça fonctionne
-    setPlayerData(response.data);
+    // ça fonctionne*
+    setPlayerData(response.data)
+    axios.get("https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/"+ response.data.id + "?api_key=" + API_KEY).then(function (resp){
+      setPlayerRank(resp.data[0])
+    })
   }).catch(function (error){
     // il y a une erreur
     console.log(error);
     setPlayerData({});
   });
+  
 }
 
   return (
@@ -31,8 +37,9 @@ function searchForPlayer (event){
       </div>
       {JSON.stringify(playerData) != '{}' ? 
         <> <p> Pseudo : {playerData.name}</p>
-           <p>Niveau : {playerData.summonerLevel}</p>
            <img width="100" height="100" src={"https://ddragon.leagueoflegends.com/cdn/14.6.1/img/profileicon/" + playerData.profileIconId +  ".png"}/>
+           <p>Niveau : {playerData.summonerLevel}</p>
+           <p>Rank : {playerRank.tier}</p>
         </> 
         : 
         <> <p>Il n'y a pas de données sur le joueur</p> </>
