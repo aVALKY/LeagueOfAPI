9b322c67a03ad1a98a129458ad3d881bb01bc9b5
aVALKY <Fillierelouis1@gmail.com>
2024-03-29 14:43:54 +0100

    La recherche de joueur est maintenent possible

--- src/App.js
+++ src/App.js
@@ -6,13 +6,20 @@ import './App.css';
 
 function App() {
 const [SearchText, setSearchText] = useState("");
+const [playerData, setPlayerData] = useState({});
 const API_KEY ="RGAPI-348637b2-be89-4132-89a1-b6bbf0c27132"
 
 function searchForPlayer (event){
   var APIcall = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ SearchText + "?api_key=" + API_KEY;
+  
   axios.get(APIcall).then(function (response) {
-
-  }).catch()
+    // ça fonctionne
+    setPlayerData(response.data);
+  }).catch(function (error){
+    // il y a une erreur
+    console.log(error);
+    setPlayerData({});
+  });
 }
 
   return (
@@ -20,9 +27,13 @@ function searchForPlayer (event){
       <div className="container">
         <h2>League of Legends Recherche de joueurs</h2>
         <input type="text" onChange={e => setSearchText(e.target.value)}></input>
-        <button onClick={e => searchForPlayer(e)}>Rechercher</button>
+        <button onClick={(e) => {searchForPlayer(e)}}>Rechercher</button>
       </div>
-
+      {JSON.stringify(playerData) != '{}' ? 
+        <> <p>Il y a des données sur le joueur</p> </> 
+        : 
+        <> <p>Il n'y a pas de données sur le joueur</p> </>
+      }
     </div>
   );
 }
